5c4e2a61228c38eb59f518949f5ad07231174be7
chuck francis <chuck_francis10@yahoo.com>
2020-10-08 12:23:37 -0400

    Add project files.

---
+++ Person class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Person_class
+{
+    class Person
+    {
+        public string name;
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+            }
+        }
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"Hi, I'm {name}");
+        }
+    }
+}
